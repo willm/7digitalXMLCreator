a3be8c2c7a8eaca5728ef712cb2103ae85368c64
willm <william.munn@7digital.com>
2011-06-14 13:57:43 +0100

    more tests

--- Tests/XMLTests.java
+++ Tests/XMLTests.java
@@ -36,7 +36,7 @@ public class XMLTests extends TestCase {
 	public void setUp(){
 		xml.printXml();
 		String filePath = "c:\\Stuff\\XMLCreator\\xml\\"+ _upc + ".xml";
-		filePath = "/home/will/Documents/Java/7digitalXMLCreator/xml/"+ _upc + ".xml";
+		//filePath = "/home/will/Documents/Java/7digitalXMLCreator/xml/"+ _upc + ".xml";
 		doc = DOMElements.parse(filePath);
 	}
 	
@@ -47,10 +47,18 @@ public class XMLTests extends TestCase {
 		assertTrue(products.getLength() == 1);
 	}
 	
-	public void test_xml_contains_distributor_node(){
-		NodeList distributorNode = getTagsByName(doc,"distributor");
+	public void test_products_contains_product_node()
+	{		
+		NodeList product = getProductElement();
 		
-		assertTrue(distributorNode.getLength() == 1);
+		assertTrue(product.getLength() == 1);
+	}
+	
+	public void test_products_contain_distributor(){
+		NodeList products = getTagsByName(doc, "products");
+		NodeList distributor = ((Element)products.item(0)).getElementsByTagName("distributor");
+		
+		assertTrue(distributor.getLength() == 1);
 	}
 	
 	public void test_distributor_contains_correct_distributor_value(){
@@ -60,114 +68,145 @@ public class XMLTests extends TestCase {
 		assertTrue(value.getTextContent().equals(_distributor));
 	}
 	
-	public void test_xml_contains_product_node(){
-		NodeList product = getTagsByName(doc,"product");
-		
-		assertTrue(product.getLength() == 1);
-	}
-	
-	public void test_productNode_contains_upc_attribute(){
-		NodeList productNode = getTagsByName(doc,"product");
+	public void productNode_contains_upc_attribute(){
+		NodeList productNode = getProductElement();
 		Attr upcAtribute = ((Element)productNode.item(0)).getAttributeNode("upc");
 		
 		assertTrue(upcAtribute != null);
 	}
 	
 	public void test_upc_attribute_contains_correct_value(){
-		NodeList productNode = getTagsByName(doc,"product");
+		NodeList productNode = getProductElement();
 		Attr upcAtribute = ((Element)productNode.item(0)).getAttributeNode("upc");
 		
 		assertTrue(upcAtribute.getNodeValue().equals(_upc));
 	}
 	
 	public void test_xml_contains_product_type_node(){
-		NodeList product_type = getTagsByName(doc,"product_type");
+		NodeList product_type = getElementFromProduct("product_type");
 		
 		assertTrue(product_type.getLength() == 1);
 	}
 	
 	public void test_product_type_contains_correct_value(){
-		NodeList product_type = getTagsByName(doc,"product_type");
+		NodeList product_type = getElementFromProduct("product_type");
 		Element value = (Element)product_type.item(0);
 		
 		assertEquals(value.getTextContent(),"Audio");
 	}
 	
 	public void test_xml_contains_product_label_node(){
-		NodeList product_label = getTagsByName(doc,"product_label");
+		NodeList product_label = getElementFromProduct("product_label");
 		
 		assertTrue(product_label.getLength() == 1);
 	}
 	
 	public void test_product_label_contains_correct_value(){
-		NodeList product_label = getTagsByName(doc,"product_label");
+		NodeList product_label = getElementFromProduct("product_label");
 		Element value = (Element)product_label.item(0);
 		
 		assertEquals(value.getTextContent(),_label);
 	}
 	
 	public void test_xml_contains_product_image_node(){
-		NodeList product_image = getTagsByName(doc,"product_image");
+		NodeList product_image = getElementFromProduct("product_image");
 		
 		assertTrue(product_image.getLength() == 1);
 	}
 	
 	public void test_product_image_contains_correct_value(){
-		NodeList product_image = getTagsByName(doc,"product_image");
+		NodeList product_image = getElementFromProduct("product_image");
 		Element value = (Element)product_image.item(0);
 		
 		assertEquals(value.getTextContent(),_image);
 	}
 	
 	public void test_xml_contains_product_title_node(){
-		NodeList product_title = getTagsByName(doc,"product_title");
+		NodeList product_title = getElementFromProduct("product_title");
 		
 		assertTrue(product_title.getLength() == 1);
 	}
 	
 	public void test_product_title_contains_correct_value(){
-		NodeList product_title = getTagsByName(doc,"product_title");
+		NodeList product_title = getElementFromProduct("product_title");
 		Element value = (Element)product_title.item(0);
 		
 		assertEquals(value.getTextContent(),_album);
 	}
 	
 	public void test_xml_contains_product_release_date_node(){
-		NodeList product_release_date = getTagsByName(doc,"product_release_date");
+		NodeList product_release_date = getElementFromProduct("product_release_date");
 		
 		assertTrue(product_release_date.getLength() == 1);
 	}
 	
 	public void test_product_release_date_contains_correct_value(){
-		NodeList product_release_date = getTagsByName(doc,"product_release_date");
+		NodeList product_release_date = getElementFromProduct("product_release_date");
 		Element value = (Element)product_release_date.item(0);
 		
 		assertEquals(value.getTextContent(),_releaseDate);
 	}
 	
 	public void test_xml_contains_product_p_line_node(){
-		NodeList productPublisherLine = getTagsByName(doc,"product_p_line");
+		NodeList productPublisherLine = getElementFromProduct("product_p_line");
 		
 		assertTrue(productPublisherLine.getLength() == 1);
 	}
 	
 	public void test_product_p_line_contains_correct_value(){
-		NodeList productPublisherLine = getTagsByName(doc,"product_p_line");
+		NodeList productPublisherLine = getElementFromProduct("product_p_line");
 		Element value = (Element)productPublisherLine.item(0);
 		
 		assertEquals(value.getTextContent(),_publisherLine);
 	}
 	
+	public void test_xml_contains_product_c_line_node(){
+		NodeList productCopyrightLine = getElementFromProduct("product_c_line");
+		
+		assertTrue(productCopyrightLine.getLength() == 1);
+	}
+	
+	public void test_product_c_line_contains_correct_value(){
+		NodeList productCopyrightLine = getElementFromProduct("product_c_line");
+		Element value = (Element)productCopyrightLine.item(0);
+		
+		assertEquals(value.getTextContent(),_copyrightLine);
+	}
+	
+	public void test_product_contains_product_artists_node(){
+		NodeList productArtistsNode = getElementFromProduct("product_artists");
+		
+		assertTrue(productArtistsNode.getLength() == 1);
+	}
+	
+	public void test_product_artists_contains_product_artist_name_node(){
+		NodeList productArtistNameNode = getElementFromArtistsNode("product_artist_name");
+		
+		assertTrue(productArtistNameNode.getLength() == 1);
+	}
+	
+
+	
+	
+	//-------------------------------------------------------------------------
+	
+	public NodeList getProductElement() {
+		NodeList products =getTagsByName(doc, "products");
+		return ((Element)products.item(0)).getElementsByTagName("product");
+	}
 	
 	public NodeList getTagsByName(Document doc, String name){
 	// Get a list of all elements in the document
 	   NodeList list = doc.getElementsByTagName(name);
 	   return list;
-	   /*System.out.println("XML Elements: ");
-	   for (int i=0; i<list.getLength(); i++) {
-		   // Get element
-		   Element element = (Element)list.item(i);
-		   System.out.println(element.getNodeName() +" : " + element.getNodeType());
-	   }*/
+	}
+	
+	public NodeList getElementFromProduct(String name) {
+		return ((Element)getProductElement().item(0)).getElementsByTagName(name);
+	}
+	
+	public NodeList getElementFromArtistsNode(String name){
+		NodeList productArtistsNode = getElementFromProduct("product_artists");
+		return ((Element)productArtistsNode.item(0)).getElementsByTagName(name);
 	}
 }
